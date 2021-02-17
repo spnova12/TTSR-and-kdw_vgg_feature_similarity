297a379a3f9180dc5c00770b5691323cc1125dc2
kdw <spnova12@gmail.com>
2021-02-17 20:25:16 +0900

    gray scale 인 경우 (아직 중간 norm 하는 것 안함)

--- mytest/my_relevance_embedding/demo.py
+++ mytest/my_relevance_embedding/demo.py
@@ -13,7 +13,7 @@ import cv2
 import matplotlib.pyplot as plt
 
 
-def tensor2pil(tensor):
+def tensor2cv2(tensor):
     # 3 channel 영상을 RGB 영상으로 만들어준다.
     # 또는 1 channel 영상을 grayscale 영상으로 만들어준다.
 
@@ -27,35 +27,21 @@ def tensor2pil(tensor):
 
     # 3 channel 일 경우
     if len(npimg.shape) == 3:
-        # tensor 를 pil 로 바꾸기 위해 channel 순서를 바꿔줌 (channel, w, h) -> (w, h, channel)
+        # tensor 를 cv2 로 바꾸기 위해 channel 순서를 바꿔줌 (channel, w, h) -> (w, h, channel)
         npimg = np.transpose(npimg, (1, 2, 0))
-        # numpy to pil
-        img_recon = Image.fromarray(np.uint8(npimg))
-
     # 1 channel 영상일 경우
-    else:
-        # numpy to pil
-        img_recon = Image.fromarray(np.uint8(npimg), 'L')
 
-    return img_recon
+    return npimg
 
 
 from sklearn.feature_extraction import image
 
 
-def RGB_patches_extractor(img, window_size): # must color image
-    [M, N, _] = img.shape
-    img_B = np.pad(img[:,:,0], [int(window_size/2), int(window_size/2)], mode='constant', constant_values=0)
-    img_G = np.pad(img[:,:,1], [int(window_size/2), int(window_size/2)], mode='constant', constant_values=0)
-    img_R = np.pad(img[:,:,2], [int(window_size/2), int(window_size/2)], mode='constant', constant_values=0)
-    img_patches_B = image.extract_patches_2d(img_B, (window_size, window_size))
-    img_patches_G = image.extract_patches_2d(img_G, (window_size, window_size))
-    img_patches_R = image.extract_patches_2d(img_R, (window_size, window_size))
-    bbb = img_patches_B.reshape(M*N, window_size,window_size, 1)
-    ggg = img_patches_G.reshape(M*N, window_size,window_size, 1)
-    rrr = img_patches_R.reshape(M*N, window_size,window_size, 1)
-    rgb_patches = np.concatenate((rrr,ggg,bbb),axis=3)
-    return rgb_patches
+def gray_patches_extractor(img, window_size): # must color image
+    M, N = img.shape
+    img = np.pad(img, [int(window_size/2), int(window_size/2)], mode='constant', constant_values=0)
+    img_patches = image.extract_patches_2d(img, (window_size, window_size))
+    return img_patches
 
 
 ###################################
@@ -64,7 +50,7 @@ if __name__ == "__main__":
     scale_factor = 2
 
     # 사용할 영상을 읽어준다.
-    img = cv2.imread("img_002.png", cv2.IMREAD_COLOR)
+    img = cv2.imread("img_002.png", cv2.IMREAD_GRAYSCALE)
     print('입력 영상 사이즈 :', img.shape)
 
     # scale factor 에 맞게 ref 영상을 만들어준다.
@@ -75,15 +61,14 @@ if __name__ == "__main__":
     win_size = 63
 
     # img_ref 를 win size 로 잘라서 Pool 을 만들어 준다.
-    pool_patches = RGB_patches_extractor(img_ref, win_size)
+    pool_patches = gray_patches_extractor(img_ref, win_size)
     print('pool_patches.shape :', pool_patches.shape)
 
     # pool 의 patch 한장을 저장해본다.
-    cv2.imwrite('patch_sample.png', pool_patches[0])
+    cv2.imwrite('pool_patches_sample.png', pool_patches[0])
 
     # pool_patches 를 tensor 로 만들어준다.
     pool_patches = torch.from_numpy(np.array(pool_patches, np.float32, copy=False))
-    pool_patches = pool_patches.permute((0, 3, 1, 2)).contiguous()
     pool_patches_tensor = pool_patches.float().div(255)
     print('pool_patches_tensor.shape :', pool_patches_tensor.shape)
 
@@ -93,35 +78,30 @@ if __name__ == "__main__":
     print('img_tensor.shape :', img_tensor.shape)
 
     # img_tensor 에서 window 를 하나 뽑아본다.
-    x_position = 0
-    y_position = 0
-    img_tensor_patch = img_tensor[..., x_position:x_position+win_size, 0:63]
+    x_position = 100
+    y_position = 100
+    img_tensor_patch = img_tensor[..., x_position:x_position+win_size, y_position:y_position+win_size]
     print('img_tensor_patch.shape :', img_tensor_patch.shape)
 
+    # img_tensor_patch 한번 저장해본다.
+    cv2.imwrite('img_tensor_patch.png', tensor2cv2(img_tensor_patch))
+
+    # https://pytorch.org/docs/stable/nn.functional.html#conv2d
+    # input - input tensor of shape : (minibatch, in_channels, iH, iW)
+    # weight - filters of shape : (out_channels, in_channels/groups, kH, kW))
+    _input = img_tensor_patch.unsqueeze(0)  # [1, 1, win_size, win_size]
+    _weight = pool_patches_tensor.unsqueeze(1)  # [pool_size, 1, win_size, win_size]
+    print(_input.shape)
+    print(_weight.shape)
+    similarity_score = F.conv2d(_input, _weight)
+    print(similarity_score.shape)
+    similarity_score = torch.squeeze(similarity_score)
+    
+    # 가장 conv 연산 결과가 큰놈
+    similarity_score_argmax = torch.argmax(similarity_score)  # (1, pool_size, 1, 1)
+    print(similarity_score_argmax)
 
 
-###
-    class inner_product_by_conv(nn.Module):
-        def __init__(self, win_size, img_tensor_patch):
-            super(inner_product_by_conv, self).__init__()
-            self.conv1 = nn.Conv2d(3, win_size, win_size)
-
-            K = img_tensor_patch
-            print(K.shape)
-            K = torch.unsqueeze(torch.unsqueeze(K, 0), 0)
-            print(k.shape)
-            self.conv1.weight.data = K
-
-        def forward(self, x):
-            x = self.conv1(x)
-            return x
-
-
-    net = inner_product_by_conv(win_size=win_size, img_tensor_patch=img_tensor_patch)
-    with torch.no_grad():
-        corespondence_tensor = net(pool_patches_tensor)
-
-    print(corespondence_tensor.shape)
 
 
 
