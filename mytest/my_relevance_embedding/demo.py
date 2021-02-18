d0992a0f45eb063cfd8c3f10fe2097f7c1167529
kdw <spnova12@gmail.com>
2021-02-18 18:12:01 +0900

    temp

--- mytest/my_relevance_embedding/demo.py
+++ mytest/my_relevance_embedding/demo.py
@@ -12,12 +12,45 @@ import cv2
 from sklearn.feature_extraction import image
 
 
+# pretrained vgg19 사용하는 방식은 https://github.com/researchmm/TTSR 에서 참고함.
+class Vgg19FeatureExtractor(torch.nn.Module):
+    def __init__(self, vgg_range=12, rgb_range=1):
+        super(Vgg19FeatureExtractor, self).__init__()
+
+        self.vgg_range = vgg_range
+
+        if self.vgg_range > 0:
+            # use vgg19 weights to initialize
+            vgg_pretrained_features = models.vgg19(pretrained=True).features
+            self.slice1 = torch.nn.Sequential()
+
+            for x in range(self.vgg_range):
+                self.slice1.add_module(str(x), vgg_pretrained_features[x])
+
+            # 학습을 시켜주지 않기때문에 grad 를 모두 없앤다.
+            for param in self.slice1.parameters():
+                param.requires_grad = False
+
+            vgg_mean = (0.485, 0.456, 0.406)
+            vgg_std = (0.229 * rgb_range, 0.224 * rgb_range, 0.225 * rgb_range)
+            self.sub_mean = MeanShift(rgb_range, vgg_mean, vgg_std)
+
+    def forward(self, x):
+        if self.vgg_range > 0:
+            x = self.sub_mean(x)
+            x = self.slice1(x)
+            x_lv1 = x
+        else:
+            x_lv1 = x
+        return x_lv1
+
+
 def tensor2cv2(tensor):
     # 3 channel 영상을 RGB 영상으로 만들어준다.
     # 또는 1 channel 영상을 grayscale 영상으로 만들어준다.
 
     # 0,1 -> 0,255
-    npimg = tensor.numpy() * 255
+    npimg = tensor.cpu().numpy() * 255
 
     # 영상 후처리.
     npimg = np.around(npimg)
@@ -33,7 +66,7 @@ def tensor2cv2(tensor):
     return npimg
 
 
-def tensor_bound_box(t, top_left, bottom_right, bgr):
+def img_tensor_bound_box(t, top_left, bottom_right, bgr):
     # 색 정보를 담고있는 tensor 를 만들어준다.
     bgr_tensor = torch.tensor([[bgr[0]], [bgr[1]], [bgr[2]]])  # [3, 1]
     bgr_tensor = bgr_tensor.expand(3, bottom_right[1]-top_left[1])  # right_bottom[1]-left_top[1] 는 윈도우 사이즈.
@@ -50,80 +83,111 @@ def tensor_bound_box(t, top_left, bottom_right, bgr):
     return img_tensor_marked
 
 
-def RGB_patches_extractor(img, window_size): # must color image
-    [M, N, _] = img.shape
-    img_B = np.pad(img[:,:,0], [int(window_size/2), int(window_size/2)], mode='constant', constant_values=0)
-    img_G = np.pad(img[:,:,1], [int(window_size/2), int(window_size/2)], mode='constant', constant_values=0)
-    img_R = np.pad(img[:,:,2], [int(window_size/2), int(window_size/2)], mode='constant', constant_values=0)
-    img_patches_B = image.extract_patches_2d(img_B, (window_size, window_size))
-    img_patches_G = image.extract_patches_2d(img_G, (window_size, window_size))
-    img_patches_R = image.extract_patches_2d(img_R, (window_size, window_size))
-    bbb = img_patches_B.reshape(M*N, window_size,window_size, 1)
-    ggg = img_patches_G.reshape(M*N, window_size,window_size, 1)
-    rrr = img_patches_R.reshape(M*N, window_size,window_size, 1)
+def RGB_patch_tensors_extractor(img_tensor, window_size): # must color image
+    print("RGB_patche_tensors_extractor===============================")
+    print(img_tensor.shape)
+    channel = img_tensor.shape[1]
+    img_tensor_unfold = F.unfold(img_tensor, kernel_size=(window_size, window_size), padding=window_size//2).squeeze()
+    print(img_tensor_unfold.shape)
+    img_tensor_unfold = img_tensor_unfold.permute((1, 0))
+    print(img_tensor_unfold.shape)
+    patches = img_tensor_unfold.view(img_tensor_unfold.shape[0],
+                                     channel,
+                                     window_size,
+                                     window_size)
+    print(patches.shape)
+    print("===========================================================")
+    return patches
 
-    # 이 코드에서는 cv2 형태로 진행할 것이기 때문에 bgr 로 순서 통일해줌.
-    rgb_patches = np.concatenate((bbb, ggg, rrr), axis=3)
-    return rgb_patches
-
-
-############################################################################################################
 
 if __name__ == "__main__":
+    ############################################################################################################
     # scale factor
     scale_factor = 2
 
     # 사용할 영상을 읽어준다.
-    img = cv2.imread("img_067.png", cv2.IMREAD_COLOR)
+    img = cv2.imread("img_098.png", cv2.IMREAD_COLOR)
     print('입력 영상 사이즈 :', img.shape)
 
     # scale factor 에 맞게 ref 영상을 만들어준다.
     img_ref = cv2.resize(img, None, fx=1/scale_factor, fy=1/scale_factor, interpolation=cv2.INTER_LANCZOS4)
     print('ref 영상 사이즈 :', img_ref.shape)
-    # cv2.imwrite('img_ref.png', img_ref)
+    cv2.imwrite('img_ref.png', img_ref)
 
-    # sliding window size. 홀수여여 됨.
+    # sliding window size. 홀수 사용.
     win_size = 63
 
-    # img_ref 를 win size 로 잘라서 Pool 을 만들어 준다.
-    pool_patches = RGB_patches_extractor(img_ref, win_size)
-    print('pool_patches.shape :', pool_patches.shape)
-
-    # pool 의 patch 한장을 저장해본다.
-    # cv2.imwrite('pool_patches_sample.png', pool_patches[0])
-
-    # pool_patches 를 tensor 로 만들어준다.
-    pool_patches = torch.from_numpy(np.array(pool_patches, np.float32, copy=False))
-    pool_patches = pool_patches.permute((0, 3, 1, 2)).contiguous()
-    pool_patches_tensor = pool_patches.float().div(255)
-    print('pool_patches_tensor.shape :', pool_patches_tensor.shape)
-
-    # cv2 image to tensor
     totensor = transforms.ToTensor()  # 이 함수를 통해 영상이 0~1 사이로 normalized 됨.
+
     img_tensor = totensor(img)
     print('img_tensor.shape :', img_tensor.shape)
 
+    img_ref_tensor = totensor(img_ref)
+    print('img_ref_tensor.shape :', img_ref_tensor.shape)
+
+    ############################################################################################################
+
+    # vgg19 feature extractor
+    vgg_range = 12
+    Vgg19FeatureExtractor = Vgg19FeatureExtractor(vgg_range=vgg_range, rgb_range=1).cuda()
+
+    # vgg feature 에서 win size 를 즉정한다. 측정을 위해 win_size 크기의 임의의 패치를 모델에 통과시켜서 output size 를 관찰한다.
+    temp_patch = torch.zeros(3, win_size, win_size)  # vgg19 의 입력은 3 channel 이다.
+    print('temp_patch.shape :', temp_patch.shape)
+    vgg_win_size = Vgg19FeatureExtractor(temp_patch.unsqueeze(0).cuda()).shape[-1]
+    if vgg_win_size % 2 == 0:  # win size 가 홀수인게 편한 듯 하다.-> 짝수면 홀수로 바꿔줌.
+        vgg_win_size += 1
+    print('vgg_win_size :', vgg_win_size)
+
+    # img_tensor 의 feature 추출. conv 연산을 하려면 [batch, channel, h, w] 형태여야 하기때문에 unsqueeze 사용.
+    img_tensor_feature = Vgg19FeatureExtractor(img_tensor.unsqueeze(0).cuda())
+    print('img_tensor_feature.shape :', img_tensor_feature.shape)
+
+    # img_ref_tensor 의 feature 추출.
+    img_ref_tensor_feature = Vgg19FeatureExtractor(img_ref_tensor.unsqueeze(0).cuda())
+    print('img_ref_tensor_feature.shape :', img_ref_tensor_feature.shape)
+
+    ############################################################################################################
+
+    # img_ref_tensor_feature 를 vgg_win_size 로 잘라서 Pool 을 만들어 준다.
+    pool_patches_tensor = RGB_patch_tensors_extractor(img_ref_tensor_feature, vgg_win_size)
+    print('pool_patches_tensor.shape :', pool_patches_tensor.shape)
+
+    # pool 의 patch 한장을 저장해본다. pool 이 생각대로 잘 만들어지고 있음을 확인할 수 있음.
+    if vgg_range == 0:
+        cv2.imwrite('pool_patches_sample.png', tensor2cv2(pool_patches_tensor[10000]))
+
+    ############################################################################################################
+
     # img_tensor 에서 window 를 하나 뽑아본다.
-    h_position = 100
-    w_position = 110
+    h_position = 500
+    w_position = 800
     img_tensor_patch = img_tensor[..., h_position:h_position+win_size, w_position:w_position+win_size]
     print('img_tensor_patch.shape :', img_tensor_patch.shape)
 
     # 원본 영상에 이 위치를 표시해보자.
-    img_tensor_marked = tensor_bound_box(img_tensor,
-                                         top_left=(h_position, w_position),
-                                         bottom_right=(h_position+win_size, w_position+win_size),
-                                         bgr=(0, 0, 1)
-                                         )
+    img_tensor_marked = img_tensor_bound_box(img_tensor,
+                                             top_left=(h_position, w_position),
+                                             bottom_right=(h_position+win_size, w_position+win_size),
+                                             bgr=(0, 0, 1)
+                                             )
     cv2.imwrite('img_tensor_marked.png', tensor2cv2(img_tensor_marked))
 
     # img_tensor_patch 한번 저장해본다.
     cv2.imwrite('img_tensor_patch.png', tensor2cv2(img_tensor_patch))
 
-    ############################################################################################################
+    # 실제 feature 단에서 crop 해야되는 위치를 계산하고, feature 단에서 patch 를 만들어준다.
+    h_feature_position = int(h_position * (vgg_win_size/win_size))
+    w_feature_position = int(w_position * (vgg_win_size/win_size))
+    img_tensor_feature_patch = img_tensor_feature[
+                               ...,
+                               h_feature_position:h_feature_position+vgg_win_size,
+                               w_feature_position:w_feature_position+vgg_win_size]
+    print('img_tensor_feature_patch.shape :', img_tensor_feature_patch.shape)
 
+    ############################################################################################################
     # img_tensor_patch 를 길이가 1인 patch 로 만들어준다.
-    img_tensor_patch_norm = img_tensor_patch / torch.sqrt(torch.sum(img_tensor_patch**2))
+    img_tensor_feature_patch_norm = img_tensor_feature_patch / torch.sqrt(torch.sum(img_tensor_feature_patch**2))
 
     # pool_patches_tensor 를 길이가 1인 patch 들로 만들어준다.
     _pool_patches_tensor = torch.sqrt(torch.sum(pool_patches_tensor**2, dim=(1, 2, 3)))
@@ -141,8 +205,8 @@ if __name__ == "__main__":
     # https://pytorch.org/docs/stable/nn.functional.html#conv2d
     # input -> input tensor of shape : (minibatch, in_channels, iH, iW)
     # weight -> filters of shape : (out_channels, in_channels, kH, kW))
-    _input = img_tensor_patch_norm.unsqueeze(0)  # [1, 3, win_size, win_size]
-    _weight = pool_patches_tensor_norm  # [pool_size, 3, win_size, win_size]
+    _input = img_tensor_feature_patch_norm  # [1, C, win_size, win_size]
+    _weight = pool_patches_tensor_norm  # [pool_size, C, win_size, win_size]
 
     gpu_on = False
     # cpu 로 해도 빠른듯.
@@ -159,64 +223,28 @@ if __name__ == "__main__":
     # 가장 conv 연산 결과가 큰놈 의 index (준상이가 필요한 부분)
     similarity_score_argmax = torch.argmax(similarity_score)  # (1, pool_size, 1, 1)
     print('similarity_score_argmax :', similarity_score_argmax)
-    similar_patch = pool_patches_tensor[similarity_score_argmax]
-    cv2.imwrite('ref_tensor_patch.png', tensor2cv2(similar_patch))
 
+    ############################################################################################################
     # ref 영상에 이 위치를 표시해보자.
-    h_position = similarity_score_argmax // img_ref.shape[1]
-    w_position = similarity_score_argmax % img_ref.shape[1]
+    h_feature_position = similarity_score_argmax // img_ref_tensor_feature.shape[1]
+    w_feature_position = similarity_score_argmax % img_ref_tensor_feature.shape[1]
+
+    h_ref_position = int(h_feature_position * (win_size/vgg_win_size))
+    w_ref_position = int(w_feature_position * (win_size/vgg_win_size))
+
+    ref_tensor_similar_patch = img_ref_tensor[
+                           ...,
+                           h_ref_position:h_ref_position + win_size,
+                           w_ref_position:w_ref_position + win_size]
+
+    cv2.imwrite('ref_tensor_patch.png', tensor2cv2(ref_tensor_similar_patch))
+
+
     padding = transforms.Pad(int(win_size / 2), fill=0)
-    img_ref_tensor_marked = tensor_bound_box(padding(totensor(img_ref)),
-                                             top_left=(h_position, w_position),
-                                             bottom_right=(h_position+win_size, w_position+win_size),
-                                             bgr=(0, 0, 1)
-                                             )
+    img_ref_tensor_marked = img_tensor_bound_box(padding(totensor(img_ref)),
+                                                 top_left=(h_ref_position, w_ref_position),
+                                                 bottom_right=(h_ref_position+win_size, w_ref_position+win_size),
+                                                 bgr=(0, 0, 1)
+                                                 )
     cv2.imwrite('img_ref_tensor_marked.png', tensor2cv2(img_ref_tensor_marked))
 
-
-
-
-
-class LTE(torch.nn.Module):
-    def __init__(self, requires_grad=True, rgb_range=1):
-        super(LTE, self).__init__()
-
-        ### use vgg19 weights to initialize
-        vgg_pretrained_features = models.vgg19(pretrained=True).features
-
-        self.slice1 = torch.nn.Sequential()
-        self.slice2 = torch.nn.Sequential()
-        self.slice3 = torch.nn.Sequential()
-
-        for x in range(2):
-            self.slice1.add_module(str(x), vgg_pretrained_features[x])
-        for x in range(2, 7):
-            self.slice2.add_module(str(x), vgg_pretrained_features[x])
-        for x in range(7, 12):
-            self.slice3.add_module(str(x), vgg_pretrained_features[x])
-        if not requires_grad:
-            for param in self.slice1.parameters():
-                param.requires_grad = requires_grad
-            for param in self.slice2.parameters():
-                param.requires_grad = requires_grad
-            for param in self.slice3.parameters():
-                param.requires_grad = requires_grad
-
-        vgg_mean = (0.485, 0.456, 0.406)
-        vgg_std = (0.229 * rgb_range, 0.224 * rgb_range, 0.225 * rgb_range)
-        self.sub_mean = MeanShift(rgb_range, vgg_mean, vgg_std)
-
-    def forward(self, x):
-        print('================LTE')
-        print(x.shape)
-        x = self.sub_mean(x)
-        x = self.slice1(x)
-        x_lv1 = x
-        print(x.shape)
-        x = self.slice2(x)
-        x_lv2 = x
-        print(x.shape)
-        x = self.slice3(x)
-        x_lv3 = x
-        print(x.shape)
-        return x_lv1, x_lv2, x_lv3
